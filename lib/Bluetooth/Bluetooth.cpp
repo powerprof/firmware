602e2bd766b3b6432390f0ac0d89819aef1c9c90
ayan4m1 <andrew@bulletlogic.com>
2021-10-07 16:41:13 -0400

    add skeleton BLE code

---
+++ lib/Bluetooth/Bluetooth.cpp
@@ -0,0 +1,46 @@
+#include <Bluetooth.h>
+
+void Bluetooth::init() {
+  BLEDevice::init(BLE_DEVICE_NAME);
+  server = BLEDevice::createServer();
+  service = server->createService(BLE_SERVICE_UUID);
+  characteristics = new BLECharacteristicMap();
+
+  auto timeChar = service->createCharacteristic(
+      BLE_TIMESTAMP_CHAR_UUID, BLECharacteristic::PROPERTY_READ);
+  auto voltChar = service->createCharacteristic(
+      BLE_VOLTAGE_CHAR_UUID, BLECharacteristic::PROPERTY_READ);
+  auto currChar = service->createCharacteristic(
+      BLE_CURRENT_CHAR_UUID, BLECharacteristic::PROPERTY_READ);
+  auto powChar = service->createCharacteristic(
+      BLE_POWER_CHAR_UUID, BLECharacteristic::PROPERTY_READ);
+
+  timeChar->addDescriptor(new BLE2902());
+  voltChar->addDescriptor(new BLE2902());
+  currChar->addDescriptor(new BLE2902());
+  powChar->addDescriptor(new BLE2902());
+
+  characteristics->setByHandle(BLE_TIMESTAMP_CHAR_HANDLE, timeChar);
+  characteristics->setByHandle(BLE_VOLTAGE_CHAR_HANDLE, voltChar);
+  characteristics->setByHandle(BLE_CURRENT_CHAR_HANDLE, currChar);
+  characteristics->setByHandle(BLE_POWER_CHAR_HANDLE, powChar);
+
+  advertiser = BLEDevice::getAdvertising();
+  advertiser->addServiceUUID(BLE_SERVICE_UUID);
+  advertiser->setScanResponse(false);
+
+  BLEDevice::startAdvertising();
+}
+
+bool Bluetooth::connected() { return false; }
+
+void Bluetooth::update(Readings* readings) {
+  characteristics->getByHandle(BLE_TIMESTAMP_CHAR_HANDLE)
+      ->setValue(readings->timestamp);
+  characteristics->getByHandle(BLE_VOLTAGE_CHAR_HANDLE)
+      ->setValue(readings->voltage);
+  characteristics->getByHandle(BLE_CURRENT_CHAR_HANDLE)
+      ->setValue(readings->current);
+  characteristics->getByHandle(BLE_POWER_CHAR_HANDLE)
+      ->setValue(readings->power);
+}
---
+++ lib/Bluetooth/Bluetooth.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <BLE2902.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <MsgPack.h>
+
+#define BLE_DEVICE_NAME "PowerProf"
+#define BLE_SERVICE_UUID "08592452-d06d-45f8-9791-2282da21defd"
+#define BLE_VOLTAGE_CHAR_HANDLE 0
+#define BLE_VOLTAGE_CHAR_UUID "ab6fcda5-8754-4301-95cc-7ee92bb2987b"
+#define BLE_CURRENT_CHAR_HANDLE 1
+#define BLE_CURRENT_CHAR_UUID "59ad55ee-0fad-4b79-87d8-3109e1cb3bc0"
+#define BLE_TIMESTAMP_CHAR_HANDLE 2
+#define BLE_TIMESTAMP_CHAR_UUID "2c17c279-7d93-4bc6-95cb-e84c6e834e9c"
+#define BLE_POWER_CHAR_HANDLE 3
+#define BLE_POWER_CHAR_UUID "19e391c5-e3bd-438d-973f-6b8283f993c9"
+
+class Bluetooth {
+  static BLEAdvertising *advertiser;
+  static BLEServer *server;
+  static BLEService *service;
+  static BLECharacteristicMap *characteristics;
+
+ public:
+  static void init();
+  static bool connected();
+  static void update(Readings *readings);
+};
