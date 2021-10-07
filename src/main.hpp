b8ca46b36f61fd34e828ecbdef58c2024e282a6a
ayan4m1 <andrew@bulletlogic.com>
2021-10-07 16:08:04 -0400

    set serial baud rate to 1 or 2 mbit/s depending on hardware

--- src/main.hpp
+++ src/main.hpp
@@ -7,4 +7,8 @@
 
 #define DEBUG_MSG_APP_ID F("gufu_gud v0.3")
 
-#define SERIAL_BAUD 115200
+#if defined(ARDUINO_TEENSY)
+#define SERIAL_BAUD 2e6  // 2MBit/s
+#else
+#define SERIAL_BAUD 1e6  // 1MBit/s
+#endif
